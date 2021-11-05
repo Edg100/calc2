944765e14e72152f89234a43c2e8f3f4e1d31ac2
Edg100 <eguillen53@gmail.com>
2021-11-05 14:09:52 -0400

    updated the calculator and calculator_text python files

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,26 +1,25 @@
-# This is the increment function
+"""This is the increment function"""
 
 class Calculator:
-    # This is the Calculator class
+    """This is the Calculator class"""
 
     result = 0
 
     def get_result(self):
-        # Get Result of Calculation
+        """Get Result of Calculation"""
         return self.result
 
-    def add_number(self, NumA, NumB):
-        # Add two numbers and store the result
-        self.result = NumA + NumB
+    def add_num_ber(self, num_a, num_b):
+        """Add two num_bers and store the result"""
+        self.result = num_a + num_b
         return self.result
 
-    def subtract_number(self, NumA, NumB):
-        # Subtract two numbers and store the result
-        self.result = NumA - NumB
+    def subtract_num_ber(self, num_a, num_b):
+        """Subtract two num_bers and store the result"""
+        self.result = num_a - num_b
         return self.result
 
-    def multiply_numbers(self, NumA, NumB):
-        # Multiply two numbers and store the result
-        self.result = NumA * NumB
+    def multiply_num_bers(self, num_a, num_b):
+        """Multiply two num_bers and store the result"""
+        self.result = num_a * num_b
         return self.result
-
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,32 +2,32 @@
 from calculator.calculator import Calculator
 
 def test_calculator_result():
-    # Testing calculator result is 0
+    """Testing calculator result is 0"""
     calc = Calculator()
     assert calc.result == 0
 
 def test_calculator_add():
-    # Testing the Add function of the calculator
-    #Arrange by instantiating the calc class
+    """Testing the Add function of the calculator"""
+    """Arrange by instantiating the calc class"""
     calc = Calculator()
-    #Act by calling the method to be tested
+    """Act by calling the method to be tested"""
     result = calc.add_number(4,2)
-    #Assert that the results are correct
+    """Assert that the results are correct"""
     assert result == 6
 
 def test_calculator_get_result():
-    # Testing the Get result method of the calculator
+    """Testing the Get result method of the calculator"""
     calc = Calculator()
     assert calc.get_result() == 0
 
 def test_calculator_subtract():
-    # Testing the subtract method of the calculator
+    """Testing the subtract method of the calculator"""
     calc = Calculator()
     result= calc.subtract_number(4,7)
     assert result == -3
 
 def test_calculator_multiply():
-    # Testing  multiplication of two numbers
+    """Testing  multiplication of two numbers"""
     calc = Calculator()
     result  = calc.multiply_numbers(2,3)
     assert result == 6
