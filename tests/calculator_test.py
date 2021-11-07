f631965a9b5d7b63f6a9b3c745553484dd74b111
Edg100 <eguillen53@gmail.com>
2021-11-07 18:38:38 -0500

    Updated calculator and calculator_test files in cal_part2 branch

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,30 +1,54 @@
-"""Testing the Calculator"""
+"""Calculator Testing"""
+import pprint
+
+import pytest
+
 from calculator.calculator import Calculator
 
-def test_calculator_result():
-    """Testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+#this is how you define a function that will run each time you pass it to a test, it is called a fixture
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
 
-def test_calculator_add():
+def test_calculator_add(clear_history):
     """Testing the Add function of the calculator"""
-    calc = Calculator()
-    result = calc.add_numbers(4,2)
-    assert result == 6
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() == True
+    assert Calculator.history_count() == 0
 
-def test_calculator_subtract():
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
+
+def test_get_last_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
     """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    result= calc.subtract_numbers(4,7)
-    assert result == -3
+    assert Calculator.subtract_number(1, 2) == -1
 
-def test_calculator_multiply():
-    """Testing  multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(4,3)
-    assert result == 12
+def test_calculator_multiply(clear_history):
+    """ tests multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
