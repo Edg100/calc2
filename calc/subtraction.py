ed3a80e460dc3eedfd506dc5281973cc63d997f8
Edg100 <eguillen53@gmail.com>
2021-11-07 19:37:29 -0500

    Updated files inside Cal folder

--- calc/subtraction.py
+++ calc/subtraction.py
@@ -10,4 +10,4 @@ class Subtraction(Calculation):
     the calculation parent class"""
     def getResult(self):
         #you need to use self to reference the data contained in the instance of the object.  This is encapsulation
-        return self.value_a - self.value_b
+        return self.num_a - self.num_b
