ae9035a4c70df09527d479e18426168a233f0583
Edg100 <eguillen53@gmail.com>
2021-11-07 20:27:45 -0500

    Updated all cal files description

--- calc/addition.py
+++ calc/addition.py
@@ -6,6 +6,7 @@ from calc.calculation import Calculation
 class Addition(Calculation):
     """The addition class has one method to get the result of the the calculation A and B come from
     the calculation parent class"""
-    def getResult(self):
-        #Here the use of self is to reference the data in the object instance which is called encapsulation.
+    def get_result(self):
+        """Here the use of self is to reference the
+        data in the object instance called encapsulation."""
         return self.num_a + self.num_b
--- calc/division.py
+++ calc/division.py
@@ -3,9 +3,10 @@
 from calc.calculation import Calculation
 
 #This is the way for Division class to extend with the Calculation class
-class Addition(Calculation):
+class Division(Calculation):
     """The division class has one method to get the result of the the calculation A and B come from
     the calculation parent class"""
-    def getResult(self):
-        #Here the use of self is to reference the data in the object instance which is called encapsulation.
+    def get_result(self):
+        """Here the use of self is to reference the
+        data in the object instance called encapsulation."""
         return self.num_a / self.num_b
