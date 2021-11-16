f5177ce536ced74564bcfa25f7e88f58151613e6
Edg100 <eguillen53@gmail.com>
2021-11-16 15:12:38 -0500

    Updated calculations_history_test.py and calculator_test.py objects

--- tests/calculations_history_test.py
+++ tests/calculations_history_test.py
@@ -1,54 +1,59 @@
-"""Testing the Calculator"""
+"""Test the Calculator class"""
 import pytest
 from calc.history.calculations import Calculations
-from calc.calculations.addition import Addition
+from calc.calculations.multiplication import Multiplication
+
+# pylint: disable=line-too-long
 
 @pytest.fixture
 def clear_history_fixture():
-    """define a function that will run each time you pass it to a test, it is called a fixture"""
+    """ This is a fixure that defines a function for testing"""
     # pylint: disable=redefined-outer-name
     Calculations.clear_history()
 @pytest.fixture
-def setup_addition_calculation_fixture():
-    """define a function that will run each time you pass it to a test, it is called a fixture"""
+def setup_multiplication_calculation_fixture():
+    """ This is a fixure that defines a function for testing"""
     # pylint: disable=redefined-outer-name
-    values = (1, 2)
-    addition = Addition(values)
-    Calculations.add_calculation(addition)
+    values = (5, 2)
+    multiplication = Multiplication(values)
+    Calculations.add_calculation(multiplication)
 
-def test_add_calculation_to_history(clear_history_fixture, setup_addition_calculation_fixture):
-    """Testing clear history returns true for success"""
+def test_add_calculation_to_history(clear_history_fixture, setup_multiplication_calculation_fixture):
+    """ This is to test clear history and returns true for success"""
+    # pylint: disable=line-too-long
     # pylint: disable=unused-argument,redefined-outer-name,singleton-comparison
     assert Calculations.count_history() == 1
 
-def test_clear_calculation_history(clear_history_fixture, setup_addition_calculation_fixture):
-    """testing clear history"""
+def test_clear_calculation_history(clear_history_fixture, setup_multiplication_calculation_fixture):
+    """Testing clear history object"""
     # pylint: disable=unused-argument,redefined-outer-name,singleton-comparison
     assert Calculations.count_history() == 1
     Calculations.clear_history()
     assert Calculations.count_history() == 0
     assert Calculations.clear_history() == True
 
-def test_get_calculation(clear_history_fixture, setup_addition_calculation_fixture):
+def test_get_calculation(clear_history_fixture, setup_multiplication_calculation_fixture):
     """Testing getting a specific calculation out of the history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculations.get_calculation(0).get_result() == 3
+    assert Calculations.get_calculation(0).get_result() == 10
 
-def test_get_calc_last_result_value(clear_history_fixture, setup_addition_calculation_fixture):
+def test_get_calc_last_result_value(clear_history_fixture, setup_multiplication_calculation_fixture):
+    """Testing getting the last calculation from the history"""
+    # pylint: disable=line-too-long
+    # pylint: disable=unused-argument,redefined-outer-name,singleton-comparison
+    assert Calculations.get_last_calculation_result_value() == 10
+
+def test_get_calculation_first(clear_history_fixture, setup_multiplication_calculation_fixture):
     """Testing getting the last calculation from the history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculations.get_last_calculation_result_value() == 3
-
-def test_get_calculation_first(clear_history_fixture, setup_addition_calculation_fixture):
-    """Testing getting the last calculation from the history"""
-    # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculations.get_first_calculation().get_result() == 3
-def test_history_count(clear_history_fixture, setup_addition_calculation_fixture):
+    assert Calculations.get_first_calculation().get_result() == 10
+def test_history_count(clear_history_fixture, setup_multiplication_calculation_fixture):
     """Testing getting the count of calculations from the history"""
     # pylint: disable=unused-argument,redefined-outer-name
     assert Calculations.count_history() == 1
-def test_get_calc_last_result_object(clear_history_fixture, setup_addition_calculation_fixture):
+def test_get_calc_last_result_object(clear_history_fixture, setup_multiplication_calculation_fixture):
     """Testing getting the last calculation from the history"""
-    # pylint: disable=unused-argument,redefined-outer-name
+    # pylint: disable=line-too-long
+    # pylint: disable=unused-argument,redefined-outer-name,singleton-comparison
     #This test if it returns the last calculation as an object
-    assert isinstance(Calculations.get_last_calculation_object(), Addition)
+    assert isinstance(Calculations.get_last_calculation_object(), Multiplication)
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,40 +1,40 @@
-"""Testing the Calculator"""
+"""This is to test the Calculator Class"""
 import pytest
 from calc.calculator import Calculator
 from calc.history.calculations import Calculations
 @pytest.fixture
 def clear_history_fixture():
-    """define a function that will run each time you pass it to a test, it is called a fixture"""
+    """ This is a fixture that defines a function for testing"""
     # pylint: disable=redefined-outer-name
     Calculations.clear_history()
-#You have to add the fixture function as a parameter to the test that you want to use it with
+#Adding the fixture function as a parameter to the test that you want to use it
 def test_calculator_add_static(clear_history_fixture):
     """testing that our calculator has a static method for addition"""
     # pylint: disable=unused-argument,redefined-outer-name
-    #using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,5.0)
+    # With Tuple we can manage as much data as we need
+    my_tuple = (4.0,5.0,1.0)
     Calculator.add_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == 8.0
+    assert Calculator.get_last_result_value() == 10.0
 def test_calculator_subtract_static(clear_history_fixture):
     """Testing the subtract method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    #using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,3.0)
+    # With Tuple we can manage as much data as we need
+    my_tuple = (10.0,5.0,1.0)
     Calculator.subtract_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == -6.0
+    assert Calculator.get_last_result_value() == -16.0
 
 def test_calculator_multiply_static(clear_history_fixture):
     """Testing the multiply method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    #using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0,2.0,3.0)
+    # With Tuple we can manage as much data as we need
+    my_tuple = (7.0,2.0,2.0)
     Calculator.multiply_numbers(my_tuple)
-    assert Calculator.get_last_result_value() == 6.0
+    assert Calculator.get_last_result_value() == 28.0
 
 def test_calculator_divide_static(clear_history_fixture):
     """Testing the divide method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    # using Tuple instead of args because we can pack as much data as we need into the tuple
+    # With Tuple we can manage as much data as we need
     my_tuple = (1.0,2.0)
     Calculator.divide_numbers(my_tuple)
     assert Calculator.get_last_result_value() == 0.5
